e414b04adce61abd198d442b78c2c403b6a09ded
Raí César G. S. <rai.cesar.g.s>
2025-12-22 10:25:54 -0300

    feat: adicionar lógica no navbar e na busca

---
+++ js/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// navbar toggle
+
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+
+const navElems = [navOpenBtn, navCloseBtn];
+
+for (let i = 0; i < navElems.length; i++) {
+  navElems[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+  });
+}
+
+// search toggle
+
+const searchContainer = document.querySelector("[data-search-wrapper]");
+const searchBtn = document.querySelector("[data-search-btn]");
+
+searchBtn.addEventListener("click", function () {
+  searchContainer.classList.toggle("active");
+});
+
+
