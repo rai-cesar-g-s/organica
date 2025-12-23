433a19ce5b36972b26851271fed30a9a3d61fc5d
Raí César G. S. <rai.cesar.g.s>
2025-12-23 09:24:53 -0300

    feat: adicionar lógica de alternância na seção aside

--- js/script.js
+++ js/script.js
@@ -23,4 +23,35 @@ searchBtn.addEventListener("click", function () {
   searchContainer.classList.toggle("active");
 });
 
+// wishlist & cart toggle
 
+const panelBtns = document.querySelectorAll("[data-panel-btn]");
+const sidePanels = document.querySelectorAll("[data-side-panel]");
+
+for (let i = 0; i < panelBtns.length; i++) {
+  panelBtns[i].addEventListener("click", function () {
+
+    let clickedElemDataValue = this.dataset.panelBtn;
+
+    panelBtns.forEach(btn => {
+  console.log("btn encontrado:", btn.dataset.panelBtn);
+});
+
+
+    for (let i = 0; i < sidePanels.length; i++) {
+
+      if (clickedElemDataValue === sidePanels[i].dataset.sidePanel) {
+        sidePanels[i].classList.toggle("active");
+        console.log(
+          "btn:", this.dataset.panelBtn,
+          "panels:", [...sidePanels].map(p => p.dataset.sidePanel)
+        );
+
+      } else {
+        sidePanels[i].classList.remove("active");
+      }
+
+    }
+
+  });
+}
