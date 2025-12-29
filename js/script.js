e524222dbd3d395811901d7b6a2ea0362aae8cdf
Raí César G. S. <rai.cesar.g.s>
2025-12-28 23:44:07 -0300

    feat: adicionar lógica na página de details

--- js/script.js
+++ js/script.js
@@ -64,3 +64,19 @@ window.addEventListener("scroll", function() {
   this.window.scrollY >= 100 ? backTopBtn.classList.add("active") 
   : backTopBtn.classList.remove("active");
 });
+
+// product details page
+
+const productDisplay = document.querySelector("[data-product-display]");
+const productThumbnails = document.querySelectorAll("[data-product-thumbnail]");
+
+for (let i = 0; i < productThumbnails.length; i++) {
+  productThumbnails[i].addEventListener("click", function () {
+    productDisplay.src = this.src;
+    productDisplay.classList.add("fade-anim");
+
+    setTimeout(function () { 
+      productDisplay.classList.remove("fade-anim");
+    })
+  })
+}
